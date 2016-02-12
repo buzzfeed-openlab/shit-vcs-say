0fd1ae40b85d4d852759124a6c97dc04be1088e5
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-02-12 13:03:31 -0800

    refactor answer options into component, add touchable elements

--- index.ios.js
+++ index.ios.js
@@ -10,7 +10,15 @@ import React, {
   Text,
   Image,
   View,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Platform,
 } from 'react-native';
+
+var TouchableElement = TouchableHighlight;
+if (Platform.OS === 'android') {
+ TouchableElement = TouchableNativeFeedback;
+}
 
 const question = {
   uuid: '06f9bff5-c51e-4087-ab88-2497bb7516c3',
@@ -90,82 +98,63 @@ class Answers extends Component {
 
         <View style={styles.answerRow}>
 
-          <View style={styles.answerOption}>
-            <View>
-              <Image
-                style={styles.profilePic}
-                source={require('./assets/bigbird.png')}
-              />
-            </View>
-            <View style={styles.optionContent}>
-              <Text style={styles.optionName}>
-                {this.props.options[0].name}
-              </Text>
-              <Text style={styles.optionHandle}>
-                @{this.props.options[0].handle}
-              </Text>
-            </View>
-          </View>
+          <Option
+            name={this.props.options[0].name}
+            handle={this.props.options[0].handle}
+            image={require('./assets/bigbird.png')}
+          />
 
-          <View style={styles.answerOption}>
-            <View>
-              <Image
-                style={styles.profilePic}
-                source={require('./assets/jason.jpg')}
-              />
-            </View>
-            <View style={styles.optionContent}>
-              <Text style={styles.optionName}>
-                {this.props.options[1].name}
-              </Text>
-              <Text style={styles.optionHandle}>
-                @{this.props.options[1].handle}
-              </Text>
-            </View>
-          </View>
+          <Option
+            name={this.props.options[1].name}
+            handle={this.props.options[1].handle}
+            image={require('./assets/jason.jpg')}
+          />
 
         </View>
 
         <View style={styles.answerRow}>
 
-          <View style={styles.answerOption}>
-            <View>
-              <Image
-                style={styles.profilePic}
-                source={require('./assets/johnolilly.jpeg')}
-              />
-            </View>
-            <View style={styles.optionContent}>
-              <Text style={styles.optionName}>
-                {this.props.options[2].name}
-              </Text>
-              <Text style={styles.optionHandle}>
-                @{this.props.options[2].handle}
-              </Text>
-            </View>
-          </View>
+          <Option
+            name={this.props.options[2].name}
+            handle={this.props.options[2].handle}
+            image={require('./assets/johnolilly.jpeg')}
+          />
 
-          <View style={styles.answerOption}>
-            <View>
-              <Image
-                style={styles.profilePic}
-                source={require('./assets/benedictevans.jpeg')}
-              />
-            </View>
-            <View style={styles.optionContent}>
-              <Text style={styles.optionName}>
-                {this.props.options[3].name}
-              </Text>
-              <Text style={styles.optionHandle}>
-                @{this.props.options[3].handle}
-              </Text>
-            </View>
-          </View>
+          <Option
+            name={this.props.options[3].name}
+            handle={this.props.options[3].handle}
+            image={require('./assets/benedictevans.jpeg')}
+          />
 
         </View>
 
       </View>
     )
+  }
+}
+
+class Option extends Component {
+  render() {
+    return (
+      <TouchableElement>
+        <View style={styles.answerOption}>
+          <View>
+            <Image
+              style={styles.profilePic}
+              source={this.props.image}
+            />
+          </View>
+          <View style={styles.optionContent}>
+            <Text style={styles.optionName}>
+              {this.props.name}
+            </Text>
+            <Text style={styles.optionHandle}>
+              @{this.props.handle}
+            </Text>
+          </View>
+        </View>
+      </TouchableElement>
+    );
   }
 }
 
@@ -177,6 +166,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
+    padding: 20,
   },
   questionContent: {
     flex: 1,
