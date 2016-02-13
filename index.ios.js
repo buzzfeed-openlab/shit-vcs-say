16940ed30114c5b81f3274ea689deeb2d336bd3c
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-02-12 16:36:31 -0800

    2 => 4 spaces

--- index.ios.js
+++ index.ios.js
@@ -1,29 +1,29 @@
 'use strict';
 
 import React, {
-  AppRegistry,
-  Component,
-  Navigator,
+    AppRegistry,
+    Component,
+    Navigator,
 } from 'react-native';
 
 import QuestionScreen from './src/question-screen.js';
 
 class QuizGame extends Component {
-  render() {
-    return (
-      <Navigator
-        initialRoute={{name: 'QuestionScreen', component: QuestionScreen}}
-        configureScene={() => {
-            return Navigator.SceneConfigs.FloatFromRight;
-        }}
-        renderScene={(route, navigator) => {
-            if (route.component) {
-                return React.createElement(route.component, { navigator });
-            }
-        }}
-      />
-    );
-  }
+    render() {
+        return (
+            <Navigator
+                initialRoute={{name: 'QuestionScreen', component: QuestionScreen}}
+                configureScene={() => {
+                        return Navigator.SceneConfigs.FloatFromRight;
+                }}
+                renderScene={(route, navigator) => {
+                        if (route.component) {
+                                return React.createElement(route.component, { navigator });
+                        }
+                }}
+            />
+        );
+    }
 }
 
 AppRegistry.registerComponent('QuizGame', () => QuizGame);
