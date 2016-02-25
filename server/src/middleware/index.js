0b186ce885d89f5c16d8b54ef9db8ff171dc42e3
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-02-24 17:31:23 -0800

    screw that, just going to use the basic express template

--- server/src/middleware/index.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-export default function() {
-	var routes = Router();
-
-	// add middleware here
-
-	return routes;
-}
--- server/src/models/questions.js
+++
@@ -1,3 +0,0 @@
-// our example model is just an Array
-var questions = [];
-export default questions;
