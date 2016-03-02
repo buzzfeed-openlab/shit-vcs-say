13c5bfd2b9e90939f58cde90515b94e9f2b1cd51
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-03-01 17:06:27 -0800

    new title screen, fix path to api

--- app/src/constants.js
+++ app/src/constants.js
@@ -3,7 +3,7 @@ import RNFS from 'react-native-fs';
 
 
 const Constants = {
-    API_PATH: 'http://ec2-52-87-152-32.compute-1.amazonaws.com/',
+    API_PATH: 'http://ec2-52-87-152-32.compute-1.amazonaws.com',
     IMAGE_DIR: RNFS.DocumentDirectoryPath + '/images/',
 }
 
