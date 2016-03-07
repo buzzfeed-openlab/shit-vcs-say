5146224a0b528497f76a1e5851a98d1ca1420dbd
Westley Hennigh-Palermo <westleyargentum@gmail.com>
2016-03-07 14:10:14 -0500

    use env vars if they're defined

--- server/models/db.js
+++ server/models/db.js
@@ -1,4 +1,11 @@
 var pgp = require('pg-promise')(),
     dbconfig = require('./config.json');
 
+if (process.env.RDS_HOSTNAME) {
+    dbconfig.host = process.env.RDS_HOSTNAME;
+    dbconfig.user = process.env.RDS_USERNAME;
+    dbconfig.password = process.env.RDS_PASSWORD;
+    dbconfig.port = process.env.RDS_PORT;
+}
+
 module.exports = pgp(dbconfig);
